fcf471429e88260953afb0256db5b5b4ac5eebc3
Keven Ferreira <Keven Ferreira@KEVEN-FERREIRA>
2022-12-26 10:04:12 -0300

    Adicionar arquivos de projeto.

---
+++ EFCore.WebAPI/Data/WKContext.cs
@@ -0,0 +1,16 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class WKContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;initial catalog=WKAPI;uid=root;pwd=teste", ServerVersion.Parse("10.4.22-MariaDB"));
+        }
+    }
+}
---
+++ EFCore.WebAPI/Migrations/20221226124353_base01.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.WebAPI.Migrations
+{
+    public partial class base01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
